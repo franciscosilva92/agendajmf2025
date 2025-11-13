842e62b11377f36412497c6192bcd48c7e9dc8af
Leandro Costa <francisco.silva92@prof.ce.gov.br>
2025-11-13 13:55:29 -0300

    Agenda JMF 2025 - Concluida

--- paginas/conteudo/del-contato.php
+++ paginas/conteudo/del-contato.php
@@ -11,28 +11,41 @@ if(isset($_GET['idDel'])){
         $result->execute();
 
         $contar = $result->rowCount();
-        if ($foto != 'avatar-padrao.png') { // fogo, terra, agua e vento
-            $filePath = "../../img/cont/" . $foto;
-
-            if (file_exists($filePath)) {
-                unlink($filePath);
+        if ($contar > 0) {
+            $show = $result->fetch(PDO::FETCH_OBJ);
+            $foto = $show->foto_contatos;
+            
+            // Verifica se a imagem não é o avatar padrão
+            if ($foto != 'avatar-padrao.png') {
+                // Caminho da imagem no servidor
+                $filePath = "../../img/cont/" . $foto;
+                
+                // Verifica se o arquivo existe e o deleta
+                if (file_exists($filePath)) {
+                    unlink($filePath);
+                }
             }
-        }
-        $delete = "DELETE FROM tb_contatos WHERE id_contatos = :id";
+        // Agora, delete o registro do banco de dados    
+        $delete = "DELETE FROM tb_contatos WHERE id_contatos=:id";
         try {
             $result = $conect->prepare($delete);
             $result->bindValue(':id', $id, PDO::PARAM_INT);
             $result->execute();
 
-            if ($result->rowCount() > 0) {
+            $contar = $result->rowCount();
+            if ($contar > 0) {
                 header("Location: ../home.php");
             } else {
-                header("location: ../home.php");
+                header("Location: ../home.php");
             }
             
         } catch (PDOException $e) {
             echo "<strong>ERRO DE DELETE: </strong>" . $e->getMessage();
         } 
+        } else {
+        // Redireciona se o registro não for encontrado
+        header("Location: ../home.php");
+        }
     } catch (PDOException $e) {
         echo "<strong>ERRO DE DELETE: </strong>" . $e->getMessage();
        
--- paginas/conteudo/perfil.php
+++ paginas/conteudo/perfil.php
@@ -76,86 +76,86 @@
                 $email_antigo = $row['email_user']; // Armazena o email antigo
                 $senha_antiga = $row['senha_user']; // Armazena a senha antiga
 
-                // Verificar se existe imagem para fazer o upload
+                 // Verificar se existe imagem para fazer o upload
                 if (!empty($_FILES['foto']['name'])) {
                   $formatP = array("png", "jpg", "jpeg", "gif"); // Formatos permitidos para upload
                   $extensao = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION); // Obtém a extensão do arquivo
 
                   // Verifica se a extensão do arquivo está nos formatos permitidos
                   if (in_array($extensao, $formatP)) {
-                    $pasta = "../img/"; // Define o diretório de upload
+                    $pasta = "../img/user/"; // Define o diretório de upload
                     $temporario = $_FILES['foto']['tmp_name']; // Caminho temporário do arquivo
                     $novoNome = uniqid() . ".{$extensao}"; // Gera um nome único para o arquivo
 
                     // Excluir a imagem antiga se ela existir
                     if (file_exists($pasta . $foto_user)) {
                       unlink($pasta . $foto_user); // Remove o arquivo antigo
-                  }
-                
-                  // Move o novo arquivo para o diretório de upload
-                  if (move_uploaded_file($temporario, $pasta . $novoNome)) {
+                    }
+
+                    if (move_uploaded_file($temporario, $pasta . $novoNome)) {
                       // Sucesso no upload da nova imagem
-                  } else {
-                      $mensagem = "Erro, não foi possível fazer o upload do arquivo!"; // Mensagem de erro
+                    } else {
+                        $mensagem = "Erro, não foi possível fazer o upload do arquivo!"; // Mensagem de erro
+                        echo $mensagem;
+                    }
+                  }else {
+                    echo "Formato inválido"; // Mensagem de erro para formato de arquivo inválido
                   }
-              } else {
-                  echo "Formato inválido"; // Mensagem de erro para formato de arquivo inválido
-              }
-          } else {
-              $novoNome = $foto_user; // Mantém a foto antiga se não houver upload de nova foto
-          }
-        
-          // Verificar se a senha foi alterada
-          if (!empty($senha_nova)) {
-              $senha = password_hash($senha_nova, PASSWORD_DEFAULT); // Hash seguro para a nova senha
-          } else {
-              $senha = $senha_antiga; // Mantém a senha antiga
-          }
-        
-          // Atualizar o banco de dados
-          $update = "UPDATE tb_user SET foto_user=:foto, nome_user=:nome, email_user=:email, senha_user=:senha WHERE id_user=:id";
-          try {
-              // Prepara a consulta de atualização
-              $result = $conect->prepare($update);
-              $result->bindParam(':id', $id_user, PDO::PARAM_STR); // Vincula o ID do usuário
-              $result->bindParam(':foto', $novoNome, PDO::PARAM_STR); // Vincula o novo nome da foto
-              $result->bindParam(':nome', $nome, PDO::PARAM_STR); // Vincula o nome do usuário
-              $result->bindParam(':email', $email, PDO::PARAM_STR); // Vincula o email do usuário
-              $result->bindParam(':senha', $senha, PDO::PARAM_STR); // Vincula a senha codificada do usuário
-              $result->execute(); // Executa a consulta
+                }else {
+                  $novoNome = $foto_user; // Mantém a foto antiga se não houver upload de nova foto
+                }
+
+                // Verificar se a senha foi alterada
+                if (!empty($senha_nova)) {
+                  $senha = password_hash($senha_nova, PASSWORD_DEFAULT); // Hash seguro para a nova senha
+                } else {
+                  $senha = $senha_antiga; // Mantém a senha antiga
+                }
+
+                // Atualizar o banco de dados
+                $update = "UPDATE tb_user SET foto_user=:foto, nome_user=:nome, email_user=:email, senha_user=:senha WHERE id_user=:id";
+
+                try {
+                  // Prepara a consulta de atualização
+                  $result = $conect->prepare($update);
+                  $result->bindParam(':id', $id_user, PDO::PARAM_STR); // Vincula o ID do usuário
+                  $result->bindParam(':foto', $novoNome, PDO::PARAM_STR); // Vincula o novo nome da foto
+                  $result->bindParam(':nome', $nome, PDO::PARAM_STR); // Vincula o nome do usuário
+                  $result->bindParam(':email', $email, PDO::PARAM_STR); // Vincula o email do usuário
+                  $result->bindParam(':senha', $senha, PDO::PARAM_STR); // Vincula a senha codificada do usuário
+                  $result->execute(); // Executa a consulta
           
-              $contar = $result->rowCount(); // Conta o número de linhas afetadas
-              if ($contar > 0) { // Verifica se alguma linha foi afetada
-                  echo '<div class="container">
-                          <div class="alert alert-success alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <h5><i class="icon fas fa-check"></i> Ok !!!</h5>
-                            Perfil atualizado com sucesso.
-                          </div>
-                        </div>';
-              
-                  // Verificar se tanto o email quanto a senha foram alterados
-                  if ($email !== $email_antigo || $senha !== $senha_antiga) {
-                      header("Location: ?sair"); // Redireciona para sair se email ou senha foram alterados
-                      exit(); // Garante que o redirecionamento ocorra
+                  $contar = $result->rowCount(); // Conta o número de linhas afetadas
+                  if ($contar > 0) { // Verifica se alguma linha foi afetada
+                      echo '<div class="container">
+                              <div class="alert alert-success alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                <h5><i class="icon fas fa-check"></i> Ok !!!</h5>
+                                Perfil atualizado com sucesso.
+                              </div>
+                            </div>';
+          
+                      // Verificar se tanto o email quanto a senha foram alterados
+                      if ($email !== $email_antigo || $senha !== $senha_antiga) {
+                          header("Location: ?sair"); // Redireciona para sair se email ou senha foram alterados
+                          exit(); // Garante que o redirecionamento ocorra
+                      } else {
+                          header("Refresh: 3; home.php?acao=perfil"); // Redireciona de volta ao perfil após 3 segundos
+                          exit(); // Garante que o redirecionamento ocorra
+                      }
                   } else {
-                      header("Refresh: 3; home.php?acao=perfil"); // Redireciona de volta ao perfil após 3 segundos
-                      exit(); // Garante que o redirecionamento ocorra
+                      // Mensagem de erro se nenhum dado foi atualizado
+                      echo '<div class="alert alert-danger alert-dismissible">
+                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                              <h5><i class="icon fas fa-times"></i> Erro !!!</h5>
+                              Perfil não foi atualizado.
+                            </div>';
                   }
-              } else {
-                  // Mensagem de erro se nenhum dado foi atualizado
-                  echo '<div class="alert alert-danger alert-dismissible">
-                          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                          <h5><i class="icon fas fa-times"></i> Erro !!!</h5>
-                          Perfil não foi atualizado.
-                        </div>';
+              } catch (PDOException $e) {
+                  // Mensagem de erro para exceções PDO
+                  echo "<strong>ERRO DE PDO= </strong>" . $e->getMessage();
               }
-          } catch (PDOException $e) {
-              // Mensagem de erro para exceções PDO
-              echo "<strong>ERRO DE PDO= </strong>" . $e->getMessage();
-          }
-        }
-          
+              }
               ?>
             </div>
 </div>
